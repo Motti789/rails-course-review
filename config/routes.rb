e00b42daab60d78a69d6a7997dbcb09da2d7a1c3
Motti789 <mchillywilly@gmail.com>
2022-02-15 10:33:26 -0500

    added a sign up form and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,20 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  root 'static#home'
+  get '/signin', to: 'session#new', as: 'signin'
+  post '/signin', to: 'session#create'
+  delete '/session/', to: 'session#destroy'
+   
+  get '/signup', to: 'users#new', as: 'new_user'
+  get '/users', to:'usersts#index', as: 'users'
+  post '/users', to:'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+
   
-   root 'static#home'
-  resources :users
   resources :courses
   resources :reviews
-  resources :session
+  
 
 end
